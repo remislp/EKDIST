02ff7d969a8b5bc58a5cb88e9f71b06ea41c908e
Remis Lape <r.lape@ucl.ac.uk>
2019-05-01 21:28:43 +0100

    Read scn files; SingleChannelRecord class.

--- _version.py
+++ _version.py
@@ -1,4 +1,10 @@
-__version__ = "0.0.0"
+__version__ = "0.0.1"
+
+# Version 0.0.1
+# Released 01/05/2019
+# - ekscn.py added: reading and writing SCN format files
+# - ekrecord.py added: contains SingleChannelRecord class
+# - some tests added
 
 # Version 0.0.0
 # Released 26/04/2019
