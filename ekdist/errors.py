355dc366105dbcda1f8776758aaff54b560bba5f
remislp <r.lape@ucl.ac.uk>
2019-08-01 14:22:01 +0100

    New module 'errors'; new class 'ApproximateSD'

---
+++ ekdist/errors.py
@@ -0,0 +1,70 @@
+import math
+import numpy as np
+from numpy import linalg as nplin
+from scipy.optimize import minimize, bisect
+
+class ApproximateSD:
+    def __init__(self, theta, func, arg, delta_step=0.0001):
+        self.hessian = self.hessian_matrix(theta, func, arg, delta_step)
+        self.covariance = nplin.inv(self.hessian)
+        self.sd = np.sqrt(self.covariance.diagonal())
+        self.correlations = self.correlation_matrix(self.covariance)
+
+    def hessian_matrix(self, theta, LLfunc, args, delta_step=0.0001):
+        """ """
+        hess = np.zeros((theta.size, theta.size))
+        deltas = self.__optimal_deltas(theta, LLfunc, args, delta_step)
+        # Diagonal elements of Hessian
+        coe11 = np.array([theta.copy(), ] * theta.size) + np.diag(deltas)
+        coe33 = np.array([theta.copy(), ] * theta.size) - np.diag(deltas)
+        for i in range(theta.size):
+            hess[i, i] = ((LLfunc(coe11[i], args) - 
+                2.0 * LLfunc(theta, args) +
+                LLfunc(coe33[i], args)) / (deltas[i]  ** 2))
+        # Non diagonal elements of Hessian
+        for i in range(theta.size):
+            for j in range(theta.size):
+                coe1, coe2, coe3, coe4 = theta.copy(), theta.copy(), theta.copy(), theta.copy()
+                if i != j:                
+                    coe1[i] += deltas[i]
+                    coe1[j] += deltas[j]
+                    coe2[i] += deltas[i]
+                    coe2[j] -= deltas[j]
+                    coe3[i] -= deltas[i]
+                    coe3[j] += deltas[j]
+                    coe4[i] -= deltas[i]
+                    coe4[j] -= deltas[j]
+                    hess[i, j] = ((
+                        LLfunc(coe1, args) -
+                        LLfunc(coe2, args) -
+                        LLfunc(coe3, args) +
+                        LLfunc(coe4, args)) /
+                        (4 * deltas[i] * deltas[j]))
+        return hess
+
+    def __tune_deltas(self, theta, func, args, Lcrit, deltas, increase=True):
+        factor = [1, 2] if increase else [-1, 0.5]
+        count = 0
+        while factor[0] * func(theta + deltas, args) < factor[0] * Lcrit and count < 100:
+            deltas *= factor[1]
+            count += 1
+        return deltas
+
+    def __optimal_deltas(self, theta, LLfunc, args, step_factor=0.0001):
+        Lcrit = LLfunc(theta, args) + math.fabs(LLfunc(theta, args) * 0.005)
+        deltas = step_factor * theta
+        L = LLfunc(theta + deltas, args)
+        if L < Lcrit:
+            deltas = self.__tune_deltas(theta, LLfunc, args, Lcrit, deltas, increase=True)
+        elif L > Lcrit:
+            deltas = self.__tune_deltas(theta, LLfunc, args, Lcrit, deltas, increase=False)
+        return deltas
+
+    def correlation_matrix(self, covar):
+        correl = np.zeros((len(covar),len(covar)))
+        for i1 in range(len(covar)):
+            for j1 in range(len(covar)):
+                correl[i1,j1] = (covar[i1,j1] / 
+                    np.sqrt(np.multiply(covar[i1,i1],covar[j1,j1])))
+        return correl
+
--- tests/test_errors.py
+++ tests/test_errors.py
@@ -7,6 +7,7 @@ import numpy.testing as npt
 from numpy import linalg as nplin
 
 from ekdist import eklib
+from ekdist import errors
 
 class TestErrorCalculation:
     def setUp(self):
@@ -18,11 +19,15 @@ class TestErrorCalculation:
         self.res = minimize(self.epdf.LL, self.epdf.theta, 
                             args=self.intervals, 
                             method='Nelder-Mead')
-        self.hess = eklib.hessian(self.res.x, self.epdf.LL, self.intervals)
+
+        self.asd = errors.ApproximateSD(self.res.x, self.epdf.LL, self.intervals)
+        #self.hess = asd.hess
+        #self.hess = eklib.hessian(self.res.x, self.epdf.LL, self.intervals)
         #self.cov = eklib.covariance_matrix(self.theta, eklib.LLexp, self.intervals)
-        self.cov = nplin.inv(self.hess)
-        self.appSD = np.sqrt(self.cov.diagonal())
-        self.corr = eklib.correlation_matrix(self.cov)
+        #self.cov = asd.covariance
+        #self.cov = nplin.inv(self.hess)
+        #self.appSD = asd.sd #np.sqrt(self.cov.diagonal())
+        #self.corr = asd.correlations #eklib.correlation_matrix(self.cov)
 
     def test_infile_exists(self):
         assert os.path.isfile(self.infile)
@@ -37,19 +42,19 @@ class TestErrorCalculation:
         npt.assert_almost_equal(self.res.x, np.array([0.03700718, 1.07302608, 0.19874548]))
 
     def test_hessian(self):
-        npt.assert_almost_equal(self.hess, [[8475.49606292, -92.21425006, -626.06648917],
+        npt.assert_almost_equal(self.asd.hessian, [[8475.49606292, -92.21425006, -626.06648917],
                                             [ -92.21425006,  71.5478832,   -36.17166531],
                                             [-626.06648917, -36.17166531,  501.13966047]])
     def test_covariance(self):
-        npt.assert_almost_equal(self.cov, [[0.00013478, 0.00026864, 0.00018777],
+        npt.assert_almost_equal(self.asd.covariance, [[0.00013478, 0.00026864, 0.00018777],
                                            [0.00026864, 0.01504143, 0.00142128],
                                            [0.00018777, 0.00142128, 0.00233261]])
 
     def test_SD(self):
-        npt.assert_almost_equal(self.appSD, [0.01160948, 0.1226435,  0.04829715])
+        npt.assert_almost_equal(self.asd.sd, [0.01160948, 0.1226435,  0.04829715])
 
     def test_correlations(self):
-        npt.assert_almost_equal(self.corr, np.array([[1.        , 0.18867387, 0.33487997],
+        npt.assert_almost_equal(self.asd.correlations, np.array([[1.        , 0.18867387, 0.33487997],
                                                      [0.18867387, 1.        , 0.23994593],
                                                      [0.33487997, 0.23994593, 1.        ]]))
 
